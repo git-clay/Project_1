d8d6123fef79179abd6837197214fc39c9c7d82b
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-19 10:03:28 -0700

    sessionStorage improved

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
-
-
-var count= 6; //count to 60seconds
- // var currentTurn = 1; // even=player1 , odd=player 2
-    var retrievedTurn = sessionStorage.getItem('currentTurn'); ////example of how to retrieve sessionstorage
+var count = 2; //count to 60seconds
+// var currentTurn = 1; // even=player1 , odd=player 2
+var retrievedTurn = sessionStorage.getItem('currentTurn'); ////example of how to retrieve sessionstorage
 console.log(retrievedTurn);
-  var retrievedObject = sessionStorage.getItem('player1'); ////example of how to retrieve sessionstorage
+var retrievedP1 = sessionStorage.getItem('retrievedP1'); ////example of how to retrieve sessionstorage
+var retrievedP2 = sessionStorage.getItem('retrievedP2'); ////example of how to retrieve sessionstorage
+console.log(retrievedP1 + ' ' + retrievedP2);
 //  console.log('retrievedObject: ', JSON.parse(retrievedObject)); ///look at bottom
+var domSelector = function(element) { //function to search for id,class,or tag (element)
+  var found;
+  if (element[0] === '#') {
+    found = document.getElementById(element.slice(1, element.length));
+  } else if (element[0] === '.') {
+    found = document.getElementsByClassName(element.slice(1, element.length));
+  } else {
+    found = document.getElementsByTagName(element);
+  }
+  return found;
+};
+var board = domSelector(".htmlBoard");
+board = board[0];
 
-
- window.onload = function() {
+window.onload = function() {
 
   var scoreBoard = new Frame('scoreB', 750, 5, 125, 40);
   var sb = domSelector('.gameHeader');
-  sb=sb[0];
- // console.log(sb);
+  sb = sb[0];
+  // console.log(sb);
   sb.append(scoreBoard.tag);
   scoreBoard.tag.innerHTML = 0;
- // currentTurn++;
+  // currentTurn++;
   var board = domSelector(".htmlBoard");
   board = board[0];
-   //   console.log(currentTurn);
+  //   console.log(currentTurn);
 
-   /*____________________________Board ____________________________________________*/
+  /*____________________________Board ____________________________________________*/
   function onBoard(player, frame) {
     board.append(player.tag);
     board.append(floor.tag);
@@ -31,39 +43,16 @@ console.log(retrievedTurn);
     board.append(point2.tag);
 
   }
-   /*____________________________Turn_______________________________________________*/
+  /*____________________________Turn_______________________________________________*/
   //this is where the events during the turn will go (player onboard,setTimer,score>player,turnEnd)
   // maybe needs a button to start? or on window load
-
-
-  if (retrievedTurn%2===0) { //is even
-  var point1 = new Frame('firePoint point pointAnimate', 10, 160, 80, 114);
-  var point2 = new Frame('firePoint point pointAnimate', 360, 160, 80, 114);
-    onBoard(player1); //setTimer onBoard
-    timer(player1);
-    keyStuff(player1);
-  //  console.log(currentTurn);
-
-  } else if (retrievedTurn%2===1) {
-   point1 = new Frame('waterPoint point pointAnimate', 10, 135, 75, 129);
-   point2 = new Frame('waterPoint point pointAnimate', 360, 135, 75, 129);
-
-    onBoard(player2);
-    timer(player2);
-   // score(player2);
-    keyStuff(player2);
-  }
-  var point = [point1, point2];
- //   console.log(currentTurn);
-
-};
 
   var inputInfo = window.location.search; //drops info from previous inputs (?Player+1=c&Player+2=s)
   var user1name = inputInfo.slice(10, inputInfo.indexOf('&')); //slices ^ to get player1name
   var user2name = inputInfo.slice(inputInfo.indexOf('&') + 10); //slices to get player 2 name
-  
-  var countInterval = setInterval(timer,1000);  //1000ms===1s
-    /*____________________________Player Objects_____________________________________*/
+
+  var countInterval = setInterval(timer, 1000); //1000ms===1s
+  /*____________________________Player Objects_____________________________________*/
   function Player(id, name, x, y, score) {
     this.tag = document.createElement('div');
     this.tag.setAttribute('class', "playerAnimate");
@@ -94,30 +83,14 @@ console.log(retrievedTurn);
   var plat2 = new Frame('platform', 350, 251, 200, 50);
   var objects = [floor, plat1, plat2];
 
-  var domSelector = function(element) { //function to search for id,class,or tag (element)
-    var found;
-    if (element[0] === '#') {
-      found = document.getElementById(element.slice(1, element.length));
-    } else if (element[0] === '.') {
-      found = document.getElementsByClassName(element.slice(1, element.length));
-    } else {
-      found = document.getElementsByTagName(element);
-    }
-    return found;
-  };
-  var board = domSelector(".htmlBoard");
-  board = board[0];
-
-
   /*____________________________Objective Objects___________________________*/
   //figure out how to make unique objective items based on current player
   //var point1 = new Frame('waterPoint point pointAnimate', 10, 135, 75, 129);
- // var point2 = new Frame('waterPoint point pointAnimate', 360, 135, 75, 129);
+  // var point2 = new Frame('waterPoint point pointAnimate', 360, 135, 75, 129);
 
   /*____________________________Score_______________________________________*/
   //display (complete/total) objectives
   //maybe: OOOOO(start) >>> XOOOO(after one objective) like hearts in zelda
-
 
   /*_______________________collision & collector_________________________________*/
   function collision(player) {
@@ -147,12 +120,10 @@ console.log(retrievedTurn);
     }
   }
 
- 
- 
   /*____________________________Player Movement_____________________________________*/
   Player.prototype.moveX = function(player, currentKey) {
     var currentId = document.getElementById(player.tag.id);
-    var att=('left:' + player.x + 'px' + ';top:' + player.y + 'px');
+    var att = ('left:' + player.x + 'px' + ';top:' + player.y + 'px');
     if (currentKey === 37) { //checkes keypress for left arrow (currentKey in moveX()) left=++
       if (player.x < 0) { //checks x position of player to determin if out of bounds
         console.log('out of bounds' + player.x);
@@ -170,13 +141,13 @@ console.log(retrievedTurn);
         player.x = player.x - 20;
       } else if (collect(player) === true) {} else {
         console.log(att);
-        currentId.setAttribute('style',att);
+        currentId.setAttribute('style', att);
       }
     }
   };
   Player.prototype.moveY = function(player, currentKey) {
     var currentId = document.getElementById(player.tag.id);
-    var att=('left:' + player.x + 'px' + ';top:' + player.y + 'px');
+    var att = ('left:' + player.x + 'px' + ';top:' + player.y + 'px');
     if (currentKey === 38) { //checkes keypress for up arrow (currentKey in moveX()) top=++
       if (player.y < 0) {
         console.log('out of bounds' + player.y);
@@ -201,7 +172,7 @@ console.log(retrievedTurn);
   function keyStuff(player) {
     document.addEventListener('keydown', function(event) { //looks for a keypress>>passes # for corrasponding keyCode
       var currentKey = event.keyCode;
-      if (event.keyCode !== 91 && event.keyCode !== 82 ) {
+      if (event.keyCode !== 91 && event.keyCode !== 82) {
         event.preventDefault(); //prevents arrow from scrolling page
         switch (event.keyCode) {
           case 37: //<
@@ -226,38 +197,58 @@ console.log(retrievedTurn);
         }
         console.log('px:' + player.x + 'py:' + player.y);
 
-       
       }
     });
   }
-
-
 
   /*____________________________Timer_______________________________________*/
   //√√countdown timer
   //maybe: in a loading bar format
   //pop-up window to display score and other info about turn
-  
-  function reset() { //refreshes page
-    location.reload();
-  }
-  function timer(player){
-    count -=1;
-   // console.log(retrievedTurn);
-     if(count<0){
-   //       sessionStorage.setItem(player.id, JSON.stringify(player)); //save player info to sessionStorage
-    //      alert(player.name+'Your score was: '+player.score);          //display results
- retrievedTurn++;
- console.log(retrievedTurn);
- sessionStorage.setItem('currentTurn', JSON.stringify(retrievedTurn)); //save player info to sessionStorage
+  if (retrievedTurn % 2 === 0) { //is even
+    var point1 = new Frame('firePoint point pointAnimate', 10, 160, 80, 114);
+    var point2 = new Frame('firePoint point pointAnimate', 360, 160, 80, 114);
+    onBoard(player1); //setTimer onBoard
+    timer(player1);
+    keyStuff(player1);
+    //  console.log(currentTurn);
 
-           clearInterval(countInterval);
-           reset();
-      return;
-        }
-    
-    domSelector('#timer').innerHTML="0:"+count;
+  } else if (retrievedTurn % 2 === 1) {
+    point1 = new Frame('waterPoint point pointAnimate', 10, 135, 75, 129);
+    point2 = new Frame('waterPoint point pointAnimate', 360, 135, 75, 129);
+
+    onBoard(player2);
+    timer(player2);
+    // score(player2);
+    keyStuff(player2);
+  }
+  //   console.log(currentTurn);
+  var point = [point1, point2];
+function timer(player) {
+  count -= 1;
+  // console.log(retrievedTurn);
+  if (count < 0) {
+    sessionStorage.setItem('retrievedP1', JSON.stringify(player1)); //save player info to sessionStorage
+    sessionStorage.setItem('retrievedP2', JSON.stringify(player2)); //save player info to sessionStorage
+
+    //      alert(player.name+'Your score was: '+player.score);          //display results
+    retrievedTurn++;
+    console.log(retrievedTurn);
+    sessionStorage.setItem('currentTurn', JSON.stringify(retrievedTurn)); //save player info to sessionStorage
+
+    clearInterval(countInterval);
+    reset();
+    return;
+  }
+  domSelector('#timer').innerHTML = "0:" + count;
 }
+};
+
+function reset() { //refreshes page
+  location.reload();
+}
+
+
 
 /*document.addEventListener('keydown',function(event){      ///finds the keycode of each key pressed
   console.log(event.which);
